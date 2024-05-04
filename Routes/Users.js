9ca68d150ee0e17a707d2c8c806f2635ea34d5a9
Perry Aryee <47823480+perryaryee@users.noreply.github.com>
2024-05-04 01:42:49 +0000

    adding_user_auth_routes_and_models

---
+++ Routes/Users.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const Router = express.Router();
+
+Router.post("/signup");
+Router.post("/login");
+
+
+
+export default Router;
--- Server.js
+++ Server.js
@@ -1,10 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import UserRoutes from "./Routes/Users.js";
 
 
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/your-database-name', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 dotenv.config();
 
@@ -15,6 +25,9 @@ app.use(cors({
     origin: ["http://localhost:3000", "http://localhost:3001"],
     optionsSuccessStatus: 200,
 }));
+
+
+app.use("/api/auth", UserRoutes);
 
 
 const PORT = 8000;
