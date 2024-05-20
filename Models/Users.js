dea6521670062dfcb565e208fd152accb8fd7ab1
Perry Aryee <47823480+perryaryee@users.noreply.github.com>
2024-05-20 01:22:38 +0000

    reset_password

--- Models/Users.js
+++ Models/Users.js
@@ -5,7 +5,9 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isverified: { type: Boolean, required: false },
-    verification_code: { type: String, required: true }
+    verification_code: { type: String, required: true },
+    resetPasswordToken: { type: String, required: false },
+    resetPasswordExpires: { type: String, required: false }
 
 });
 
