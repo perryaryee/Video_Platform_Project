98041fff7006e19430863e67477fa1149061832b
Perry Aryee <47823480+perryaryee@users.noreply.github.com>
2024-06-19 02:14:00 +0000

    api_setup_corection

--- Server.js
+++ Server.js
@@ -6,44 +6,42 @@ import UserRoutes from "./Routes/Users.js";
 import VideoRoutes from "./Routes/Videos.js";
 import AdminRoutes from "./Routes/Admin.js";
 
-
-
-
 const app = express();
 
 dotenv.config();
 
-// mongoose.connect('mongodb://127.0.0.1:27017/videoplatform');
 mongoose.connect(`mongodb+srv://perryaryeesci:${process.env.DBPASSWORD}@cluster1.lwgkhy4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`);
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-
 db.once('open', () => {
     console.log('Database connected successfully');
-})
-
-
+});
 
 app.use(express.json());
 
-
 app.use(cors({
-    origin: ["http://localhost:3000","http://localhost:3000","https://asvavideoplatform.vercel.app/"],
+    origin: ["http://localhost:3000", "https://asvavideoplatform.vercel.app"],
     optionsSuccessStatus: 200,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
 }));
 
+
+app.options("*", cors({
+    origin: ["http://localhost:3000", "https://asvavideoplatform.vercel.app"],
+    optionsSuccessStatus: 200,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+}));
 
 app.use("/api/auth", UserRoutes);
 app.use("/api/video", VideoRoutes);
 app.use("/api/admin", AdminRoutes);
 app.use("/Videos", express.static("./Videos"));
 
-
 const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-
